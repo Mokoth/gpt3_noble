7d753812058e71597af7dc746b81b42c4ca99e37
Mokoth <onyach.okoth1@outlook.com>
2023-03-06 07:03:41 +0300

    add brand features

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './brand.css'
+import { google, atlassian, slack, shopify, dropbox } from './import'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__heading'>
+      <div><img src={google} alt='google'/></div>
+      <div><img src={slack} alt='slack'/></div>
+      <div><img src={dropbox} alt='dropbox'/></div>
+      <div><img src={shopify} alt='shopify'/></div>
+      <div><img src={atlassian} alt='atlassian'/></div>
+    </div>
   )
 }
 
